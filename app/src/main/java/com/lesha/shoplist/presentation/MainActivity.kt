c45cff6d7ecc5738b16f5b273fe0e319816007ab
Alexey-Lysenko <63449694+Alexey-Lysenko@users.noreply.github.com>
2022-08-29 14:40:28 +0700

    Initial commit

---
+++ app/src/main/java/com/lesha/shoplist/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.lesha.shoplist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lesha.shoplist.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
