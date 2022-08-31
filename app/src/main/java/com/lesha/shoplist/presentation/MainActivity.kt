6813456c08f57aea9e5db7f18a9308093b0cd337
Alexey-Lysenko <63449694+Alexey-Lysenko@users.noreply.github.com>
2022-08-31 19:02:15 +0700

    added adapter and setupRecyclerView function

--- app/src/main/java/com/lesha/shoplist/presentation/MainActivity.kt
+++ app/src/main/java/com/lesha/shoplist/presentation/MainActivity.kt
@@ -1,23 +1,40 @@
 package com.lesha.shoplist.presentation
 
-import android.content.ContentProvider
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.lesha.shoplist.R
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
-
-    private var count = 0
+    private lateinit var adapter: ShopListAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupRecyclerView()
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-        viewModel.shopList.observe(this){
-            Log.d("MainActivityTest",it.toString())
+        viewModel.shopList.observe(this) {
+            adapter.shopList = it
         }
     }
+
+    private fun setupRecyclerView() {
+        val recyclerViewShopList = findViewById<RecyclerView>(R.id.recycler_view_shop_list)
+        recyclerViewShopList.also {
+            adapter = ShopListAdapter()
+            it.adapter = adapter
+            it.recycledViewPool.setMaxRecycledViews(
+                ShopListAdapter.ENABLED_ITEM_TYPE,
+                ShopListAdapter.POOL_OF_VIEW_HOLDERS
+            )
+            it.recycledViewPool.setMaxRecycledViews(
+                ShopListAdapter.DISABLED_ITEM_TYPE,
+                ShopListAdapter.POOL_OF_VIEW_HOLDERS
+            )
+
+        }
+    }
+
 }
